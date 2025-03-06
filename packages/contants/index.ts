c9e1288eda4a418d12122120aa09646247011fb4
zimejin07 <zimejin@gmail.com>
2025-03-06 12:33:39 +0100

    project setup for casinos

---
+++ packages/contants/index.ts
@@ -0,0 +1,6 @@
+export const GAME_NAME = "Casino Game Portal";
+
+export const BRANDS = {
+    CASINO_A: "Casino A",
+    CASINO_B: "Casino B",
+};
---
+++ packages/types/index.ts
@@ -0,0 +1,4 @@
+export type CasinoConfig = {
+    name: string;
+    menuPosition: "left" | "right";
+};
